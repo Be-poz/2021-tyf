d285511c0f1efce5b94585533df19c80503405c2
Bepoz <45073750+Be-poz@users.noreply.github.com>
2021-07-27 19:27:49 +0900

    Update IndexController.java

--- server/src/main/java/com/example/tyfserver/IndexController.java
+++ server/src/main/java/com/example/tyfserver/IndexController.java
@@ -3,16 +3,16 @@ package com.example.tyfserver;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class IndexController {
 
     @GetMapping("/nores")
     public String asdf() {
-        return "index";
+        return "nores";
     }
 
-    @ResponseBody
     @GetMapping("/res")
     public String res() {
         return "res";
